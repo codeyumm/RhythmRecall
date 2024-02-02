f63aafd73f476a1f2bce28c69652130992fb8500
codeyumm <pvashi006@gmail.com>
2024-02-01 19:33:26 -0500

    Added operation "AddToLIstenLaterList"

--- RhythmRecall/Controllers/TrackListDataController.cs
+++ RhythmRecall/Controllers/TrackListDataController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Data.Entity;
 using System.Web.Http.Description;
+using Track = RhythmRecall.Models.Track;
 
 namespace RhythmRecall.Controllers
 {
@@ -85,9 +86,7 @@ namespace RhythmRecall.Controllers
                 listenLaterList.Add(new TrackListDto
                 {
                     Id = track.TrackId,
-
                     Title = track.Tracks.Title,
-
                     Username = track.Userss.Username
 
                 });
@@ -96,6 +95,63 @@ namespace RhythmRecall.Controllers
 
             return Ok(listenLaterList);
         }
+
+
+        // add song to listen later list
+        // we will need trackId, UserId
+        // before addig we have to check that
+        // if song and user exist or not and if user already has that song in listen later list
+        // send some error message . .
+        // else
+        // add that song to listen later list
+
+        [HttpPost]
+        [Route("api/TrackListData/AddToListenLaterList/{userId}/{trackId}")]
+
+        public IHttpActionResult AddToLIstenLaterList(int userId, int trackId)
+        {
+
+            // check track with trackId from request exist or not
+            // get track with trackId
+            // if the query returns 1 track exist if it returns 0 track doesn't exist
+            bool isTrackExist =   ( db.Tracks.Where(t => t.Id == trackId).Count() == 1 ) ? true : false;
+
+            // check if user already have the trak in thier listen later list or not
+            bool isAlreadyAdded = ( db.TrackLists.Where(track => track.TrackId == trackId).
+                                                Where( user => user.UserId == userId).Count() == 1 ) ? true : false;
+
+            // if track exist and it not in user's listen later list
+            // add that song to list
+
+            if( isTrackExist && !isAlreadyAdded)
+            {
+                Debug.WriteLine("You are good to go");
+            } else
+            {
+                Debug.WriteLine("There is some error");
+            }
+
+
+            return Ok("To add song in listen later list");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // get list of discoverd song
 
